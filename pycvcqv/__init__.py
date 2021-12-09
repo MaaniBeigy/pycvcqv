258cbb589e57fa659f350b48a3b84ac5aa071339
MaaniBeigy <manibeygi@gmail.com>
2021-12-09 23:51:13 +0330

    :arrow_down: Downgrading dependencies for python3.7

--- pycvcqv/__init__.py
+++ pycvcqv/__init__.py
@@ -1,16 +1,19 @@
 # type: ignore[attr-defined]
 """Coefficient of Variation (CV) and Coefficient of Quartile Variation (CQV)."""
-
-from importlib import metadata as importlib_metadata
+import sys
 
 from pycvcqv.cqv import cqv
 from pycvcqv.cv import coefficient_of_variation
 from pycvcqv.dataframe import cqv_dataframe, cv_dataframe
 from pycvcqv.is_numeric import is_numeric
 
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
 # -------------------------------- function definition --------------------------------
-
-
 def get_version() -> str:
     """Gets the version of package."""
     try:
