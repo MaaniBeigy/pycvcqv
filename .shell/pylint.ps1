c8621d15e6c41781ff5da04810e741c93107e031
Maani Beigy <manibeygi@gmail.com>
2024-08-11 20:50:28 +0200

    :sparkles: adding pylint-log json output

--- .shell/pylint.ps1
+++ .shell/pylint.ps1
@@ -2,7 +2,7 @@
 Get-ChildItem -Path . -Recurse -Filter 'pylint-log.txt' | Remove-Item -Force
 
 # Find and delete files named 'pylint.svg'
-Get-ChildItem -Path . -Recurse -Filter 'pylint.svg' | Remove-Item -Force
+# Get-ChildItem -Path . -Recurse -Filter 'pylint.svg' | Remove-Item -Force
 
 # Run pylint using poetry and save the output to a log file
 poetry run pylint pycvcqv | Tee-Object -FilePath ".logs/pylint-log.txt"
@@ -14,9 +14,20 @@ $lintscore = Select-String -Path ".logs/pylint-log.txt" -Pattern 'rated at' | Fo
     $_ -split '/' | Select-Object -First 1
 }
 
+# Convert lintscore to a number
+$lintscore = [double]$lintscore
+
+# Create an object to store the lintscore in JSON format
+$jsonOutput = @{
+    lintscore = $lintscore
+} | ConvertTo-Json
+
+# Save the JSON output to a file
+$jsonOutput | Set-Content -Path ".logs/pylint-log.json"
+
 # Output the lint score (for verification)
 Write-Output "lintscore: $lintscore"
 
 # Generate the pylint badge using anybadge and save it to a file
-$command = "poetry run python -m anybadge -o --value=$lintscore --file=assets/images/pylint.svg pylint"
-Invoke-Expression $command
+# $command = "poetry run python -m anybadge -o --value=$lintscore --file=assets/images/pylint.svg pylint"
+# Invoke-Expression $command
