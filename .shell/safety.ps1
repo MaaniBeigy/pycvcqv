ebec3ebf59b427e124fc6422a1a6fbae47e4a4c4
Maani Beigy <manibeygi@gmail.com>
2024-08-11 20:27:34 +0200

    :bug: fix safety json output

--- .shell/safety.ps1
+++ .shell/safety.ps1
@@ -11,9 +11,32 @@ $vulnerabilities_found = $jsonObject.report_meta.vulnerabilities_found
 Write-Output $vulnerabilities_found
 
 # Remove the old vulnerabilities.svg file
-Remove-Item -Path "assets/images/vulnerabilities.svg" -Force -Recurse
+# Remove-Item -Path "assets/images/vulnerabilities.svg" -Force -Recurse
+
+# Define the path to the input and output files
+$inputFilePath = ".logs/safety.json"
+$outputFilePath = ".logs/safety.json"
+
+# Read the content of the JSON file
+$jsonContent = Get-Content -Path $inputFilePath -Raw | ConvertFrom-Json
+
+# Extract the value from the specific path
+$vulnerabilitiesFound = $jsonContent.report_meta.vulnerabilities_found
+
+# Create a new object with the desired structure
+$result = @{
+    vulnerabilities_found = $vulnerabilitiesFound
+}
+
+# Convert the object to JSON format
+$jsonOutput = $result | ConvertTo-Json
+
+# Write the output JSON to a file
+$jsonOutput | Set-Content -Path $outputFilePath
+
+Write-Output "The JSON data has been extracted and saved to $outputFilePath"
 
 # Run the Python command using poetry and the extracted value
 # Note: Make sure 'poetry' and 'python3' are in your system's PATH
-$command = "poetry run python -m pybadges --left-text='vulnerabilities' --right-text='$vulnerabilities_found' --left-color='#40aef9' --right-color='#0c2739' --logo=assets/images/safety.png --embed-logo >> assets/images/vulnerabilities.svg"
-Invoke-Expression $command
+# $command = "poetry run python -m pybadges --left-text='vulnerabilities' --right-text='$vulnerabilities_found' --left-color='#40aef9' --right-color='#0c2739' --logo=assets/images/safety.png --embed-logo >> assets/images/vulnerabilities.svg"
+# Invoke-Expression $command
