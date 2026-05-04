99ec25e7c7c9c7ee2268cd8357467728d8028dd9
MaaniBeigy <maanibeigy@gmail.com>
2026-05-04 14:01:23 +0200

    :boom: adds mckay, vangel, mahmoudvand_hassani, equal_tailed, shortest_length, normal_approximation

--- pycvcqv/cv_confidence_interval.py
+++ pycvcqv/cv_confidence_interval.py
@@ -1,9 +1,15 @@
 """Confidence Intervals for Coefficient of Variation (cv)."""
 
 # --------------------------- Import libraries and functions --------------------------
+from pycvcqv.equal_tailed import _equal_tailed_cv_confidence_interval
 from pycvcqv.kelley import _kelley_cv_confidence_interval
+from pycvcqv.mahmoudvand_hassani import _mahmoudvand_hassani_cv_confidence_interval
+from pycvcqv.mckay import _mckay_cv_confidence_interval
 from pycvcqv.miller import _miller_cv_confidence_interval
+from pycvcqv.normal_approximation import _normal_approximation_cv_confidence_interval
+from pycvcqv.shortest_length import _shortest_length_cv_confidence_interval
 from pycvcqv.types import NumArrayLike  # custom numeric array defined in types.py.
+from pycvcqv.vangel import _vangel_cv_confidence_interval
 
 # -------------------------------- function definition --------------------------------
 
@@ -26,7 +32,13 @@ def _cv_confidence_intervals(
     # ------------- apply corresponding method for cv confidence intervals ------------
     methods = {
         "kelley": _kelley_cv_confidence_interval,
+        "mckay": _mckay_cv_confidence_interval,
         "miller": _miller_cv_confidence_interval,
+        "vangel": _vangel_cv_confidence_interval,
+        "mahmoudvand_hassani": _mahmoudvand_hassani_cv_confidence_interval,
+        "equal_tailed": _equal_tailed_cv_confidence_interval,
+        "normal_approximation": _normal_approximation_cv_confidence_interval,
+        "shortest_length": _shortest_length_cv_confidence_interval,
     }
     result: dict[str, float | int] = methods[method](
         data,
